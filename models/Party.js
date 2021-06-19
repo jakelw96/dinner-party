be2976a86ebc36c858f080ab071f66f82947c88e
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-06-19 14:18:37 -0600

    party model complete

--- models/Party.js
+++ models/Party.js
@@ -1,2 +1,30 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+
+class Bio extends Model{}
+
+Party.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+
+        }
+    
+
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'party'
+    }    
+
+
+);
