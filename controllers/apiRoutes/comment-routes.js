2cf6b6b5c58eabcb79e163eb17111241c7f10746
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-06-19 16:00:05 -0600

    router.delete delete a comment

--- controllers/apiRoutes/comment-routes.js
+++ controllers/apiRoutes/comment-routes.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
 const { Party, User} = require('../../models');
+const Comment = require('../../models/Comment');
+
 
 
 
@@ -42,3 +44,64 @@ router.get('/:id', (req,res) => {
         res.status(500).json(err);
     }));
 });
+
+//Create a comment
+router.post('/', (req,res) => {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//Update a comment
+router.put('/:id', (req,res) => {
+    Comment.update(
+        {
+            comment_text: req.body.comment_text
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+    .then(dbCommentData => {
+        if (!dbCommentData) {
+            res.status(404).json({ message: 'No comment found with this id'});
+            return;
+        }
+        res.json(dbCommentData)
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//Delete a comment
+router.delete('/:id', (req,res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    //Should we do an alert here to ask if sure they want to delete comment??//
+    // .then(dbPartyData => {
+    //     if (!dbPartyData) {
+    //         res.status(404).json({message: 'No party found with this id'});
+    //         return;
+    //     }
+    //     res.json(dbPartyData)
+    // })
+    // .catch(err => {
+    //     console.log(err);
+    //     res.status(500).json(err);
+    });
+});    
+
+module.exports = router;
