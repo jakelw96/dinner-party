aed14c0db5d35d4e5605143945f5c8cabc493755
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-06-19 15:33:44 -0600

    comment.js

--- models/Comment.js
+++ models/Comment.js
@@ -3,3 +3,32 @@ const sequelize = require('../config/connection');
 
 class Comment extends Model{}
 
+Comment.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: true
+        },
+        comment_text: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'comment'
+    }    
+);
+
+module.exports = Comment;
