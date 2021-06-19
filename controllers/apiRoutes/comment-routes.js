bad1e5568f50bf57e4677cea3f800c7edaabb154
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-06-19 15:44:09 -0600

    router.get get all comments

--- controllers/apiRoutes/comment-routes.js
+++ controllers/apiRoutes/comment-routes.js
@@ -1,3 +1,22 @@
 const router = require('express').Router();
 const { Party, User} = require('../../models');
 
+
+//Get all comments
+router.get('/', (req,res) => {
+    Comment.findAll({
+        attributes: ['id', 'comment_text', 'user_id'],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+
+    })
+    .then(dbCommentData => res.json(dbCommentData)
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    }));
+});
