30ffb549a71aed87c15f09cdd8ec436a97aa567f
Amber Pratt <78624822+apratt7891@users.noreply.github.com>
2021-06-20 12:02:25 -0600

    update router.get get all posts

--- controllers/apiRoutes/post-routes.js
+++ controllers/apiRoutes/post-routes.js
@@ -1,18 +1,49 @@
 const router = require('express').Router();
-const { User, Comment } = require('../../models');
+const { User, Party } = require('../../models');
 
 //Get all posts
 router.get('/', (req, res) => {
-    Bio.findAll({
+    Post.findAll(
+        {
+        include: [
+            {
+               model: Party,
+               attributes: ['party_name']
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+//Get one post
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
         attributes: ['id', 'post_text', 'user_id'],
         include: [
             {
-                model: Party,
+               // model: User, 
                // attributes: ['username']
             }
         ]
     })
-    .then(dbBioData => res.json(dbBioData))
+    .then(dbPostData => {
+        if (!dbBioData) {
+            res.status(404).json({ message: 'No bio found with this id' });
+            return;
+        }
+        res.json(dbBioData)
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
