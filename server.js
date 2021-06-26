8af63e811367d5ff3a3c22a2bb3348e0738dba40
jakelw96 <jake.lw96@gmail.com>
2021-06-25 22:56:16 -0600

    Set up sessions and handlebars engine in server file

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
+const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const serverRoutes = require('./controllers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: 'gfrdgdfgbh45jrh45j64jk5k456gfds',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Turns on routes
 app.use(serverRoutes);
