4312b5e3284814ad8057f3d747fe4d2682ad0769
Bobby T <bob.tarino@gmail.com>
2021-06-27 17:58:35 -0500

    installed faker dependency and created fake usernames and passwords

--- seeds/users.js
+++ seeds/users.js
@@ -1,13 +1,18 @@
+const faker = require('faker');
+
 const seedUsers = async () => {
     console.log('Seeding users...');
     let users = [];
 
     for (var i = 0; i < 10; i++) {
         let user = {
-         username: faker.name.userName(),
-         password: await bcrypt.hash('password', 10),
+         username: faker.internet.userName(),
+        //  password: await bcrypt.hash('password', 10),
+        password: faker.internet.password()
         };
         users.push(user);
        }
        return users;
 }
+
+module.exports = seedUsers; 
