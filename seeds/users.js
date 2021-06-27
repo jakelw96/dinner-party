d65419efea26e756c4ae5c447b44799c6299302e
Bobby T <bob.tarino@gmail.com>
2021-06-27 18:13:41 -0500

    added bcrypt to the seeded users function

--- seeds/users.js
+++ seeds/users.js
@@ -1,4 +1,5 @@
 const faker = require('faker');
+const bcrypt = require('bcrypt');
 
 const seedUsers = async () => {
     console.log('Seeding users...');
@@ -7,8 +8,7 @@ const seedUsers = async () => {
     for (var i = 0; i < 10; i++) {
         let user = {
          username: faker.internet.userName(),
-        //  password: await bcrypt.hash('password', 10),
-        password: faker.internet.password()
+         password: await bcrypt.hash('password', 10),
         };
         users.push(user);
        }
