37178040d14e5ac666d6722b2e0431c60bdb140e
jakelw96 <jake.lw96@gmail.com>
2021-06-27 12:18:22 -0600

    Added faker.js dependency. Updated frontend login.js to incorporate faker, added btn to signup form.

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,5 @@
+const faker = require('faker');
+
 async function loginFormHandler(event) {
     event.preventDefault();
 
@@ -48,6 +50,23 @@ async function signupFormHandler(event) {
     }
 }
 
+// Function to generate random data in the 
+async function fakerData(event) {
+    event.preventDefault();
+
+    // Creates random data to use for signing up via faker.js
+    let randomEmail = faker.internet.email();
+    let randomUsername = faker.internet.userName();
+    let randomPassword = faker.internet.password();
+
+
+    document.querySelector('#username-signup').value = randomUsername.trim();
+    document.querySelector('#email-signup').value = randomEmail.trim();
+    document.querySelector('#password-signup').value = randomPassword.trim();
+}
+
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
 
 document.querySelector('signup-form').addEventListener('submit', signupFormHandler);
+
+document.getElementById('randBtn').addEventListener('click', fakerData);
