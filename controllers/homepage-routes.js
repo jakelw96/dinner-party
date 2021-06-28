13faa0f2433d92e5a7f23758f460cba6bff3362f
jakelw96 <jake.lw96@gmail.com>
2021-06-27 19:58:51 -0600

    Added homepage routes

---
+++ controllers/homepage-routes.js
@@ -0,0 +1,105 @@
+const router = require('express').Router();
+const { User, Party, Post, Comment, Interest } = require('../models');
+
+// Gets all parties
+router.get('/', (req, res) => {
+    Party.findAll({
+        attributes: ['id', 'party_name', 'party_bio'],
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'post_name', 'post_text', 'user_id'],
+                include: [
+                    {
+                        model: Comment,
+                        attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                        include: {
+                            model: User,
+                            attributes: ['username']
+                        }
+                    },
+                    {
+                        model: User,
+                        attributes: ['username']
+                    } 
+                ]
+            },
+            {
+                model: Interest,
+                attributes: ['id', 'interest_name']
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPartyData => {
+        const parties = dbPartyData.map(party => party.get({ plain: true }));
+        res.render('homepage', {
+            parties,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// Get a single party
+router.get('/party/:id', (req, res) => {
+    Party.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: ['id', 'party_name', 'party_bio'],
+        include: [
+            {
+                model: Post,
+                attributes: ['id', 'post_name', 'post_text', 'user_id'],
+                include: [
+                    {
+                        model: Comment,
+                        attributes: ['id', 'comment_text', 'post_id', 'user_id'],
+                        include: {
+                            model: User,
+                            attributes: ['username']
+                        }
+                    },
+                    {
+                        model: User,
+                        attributes: ['username']
+                    } 
+                ]
+            },
+            {
+                model: Interest,
+                attributes: ['id', 'interest_name']
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+        
+    })
+    .then(dbPartyData => {
+        if (!dbPartyData) {
+            res.status(404).json({ message: 'No party with this id' });
+            return;
+        }
+        const party = dbPartyData.get({ plain: true });
+
+        res.render('single-party', {
+            party,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/login-routes.js
+++ controllers/login-routes.js
@@ -10,7 +10,12 @@ router.get('/', (req, res) => {
 });
 
 router.get('/signup', (req, res) => {
-    
-})
+    if (req.session.loggedIn) {
+        res.redirect('/dashboard')
+        return;
+    }
+
+    res.render('signup');
+});
 
 module.exports = router;
